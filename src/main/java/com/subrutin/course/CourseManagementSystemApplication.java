0ed449df095024547b45704f2d1f7fcad7615759
Tedy Tri Saputro <tedy@saputro.dev>
2023-11-20 13:22:59 +0700

    initial commit

---
+++ src/main/java/com/subrutin/course/CourseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.subrutin.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subrutin/course/CourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subrutin.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
